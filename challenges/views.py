2043102ac7dbb1068369be3fb8328fbfa1fbb768
amit <amit.3k3@gmail.com>
2021-09-04 13:32:42 +0530

    Dynamic Path

--- challenges/views.py
+++ challenges/views.py
@@ -4,5 +4,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello World!! \n\n\n This worked :)')
+def index(request, month):
+    challenge = ''
+    if month == 'january':
+        challenge = 'Eat no meat for 30 days'
+    elif month == 'february':
+        challenge = 'walk for 30 mins evry day'
+    elif month == 'march':
+        challenge = 'Practice Django for 30 mins everyday'
+    else:
+        challenge = 'This month is not supported'
+    return HttpResponse(challenge)
+
